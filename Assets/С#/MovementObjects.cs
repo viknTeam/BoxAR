06d96c55d9409973cf5de7946116d9c63daac914
Michail56 <potapov190403@yandex.ru>
2021-11-25 16:51:31 +0500

    03
    Добавил анимацию на Mob, добавил игрока.

--- Assets/С#/MovementObjects.cs
+++ Assets/С#/MovementObjects.cs
@@ -4,24 +4,10 @@ using UnityEngine;
 
 public class MovementObjects : MonoBehaviour
 {
-    public float speed;
+    public int S;
     // Start is called before the first frame update
     void Start()
     {
-        speed = 2;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
-        if (transform.position.x <= -1.31f)
-        {
-            speed = -speed;
-        }
-        if (transform.position.x >= 0f)
-        {
-            speed = 2;
-        }
+        
     }
 }
