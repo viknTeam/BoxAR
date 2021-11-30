3daf85a72ad5a646d2c00ca7bfa454d4249c8a23
Michail56 <potapov190403@yandex.ru>
2021-11-30 15:55:39 +0500

    03.03.10

--- Assets/С#/Movemen.cs
+++ Assets/С#/Movemen.cs
@@ -22,6 +22,10 @@ public class Movemen : MonoBehaviour
         ch_BoxAnimation = GetComponent<Animation>();
 
     }
+    private void Update()
+    {
+        BoxMove();
+    }
 
     //Метод перемещения персонажа
     private void BoxMove()
@@ -29,6 +33,14 @@ public class Movemen : MonoBehaviour
      boxVector = Vector3.zero;
      boxVector.x = Input.GetAxis("Horizontal") * speedBoxOne;
      boxVector.z = Input.GetAxis("Vertical") * speedBoxOne;
+
+        if (Vector3.Angle(Vector3.forward, boxVector) > 1f)
+        {
+            Vector3 direct = Vector3.RotateTowards(transform.forward, boxVector, speedBoxOne, 0.0f);
+            transform.rotation = Quaternion.LookRotation(direct);
+        }
+
+        ch_BoxController.Move(boxVector * Time.deltaTime);// Метод движения по направлению
 
     }
 
