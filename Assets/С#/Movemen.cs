53eaecedf3ac19c778e41bda6cd5412dff887821
Michail56 <potapov190403@yandex.ru>
2021-12-01 15:55:38 +0500

    03.03.11
    Создал ходьбу на персонажа.

--- Assets/С#/Movemen.cs
+++ Assets/С#/Movemen.cs
@@ -8,18 +8,18 @@ public class Movemen : MonoBehaviour
     public float speedBoxOne; //Скорость персонажа
 
     //Параметры гемплея персонажа
-    private float grvitiForse; //Гравитация персонажа
+    
     private Vector3 boxVector; //Напаравление персонажа
 
     //Ссылки на компоненты
     private CharacterController ch_BoxController;
-    private Animation ch_BoxAnimation;
+    private Animator ch_BoxAnimation;
 
 
     private void Start()
     {
         ch_BoxController = GetComponent<CharacterController>();
-        ch_BoxAnimation = GetComponent<Animation>();
+        ch_BoxAnimation = GetComponent<Animator>();
 
     }
     private void Update()
@@ -30,17 +30,21 @@ public class Movemen : MonoBehaviour
     //Метод перемещения персонажа
     private void BoxMove()
     {
-     boxVector = Vector3.zero;
-     boxVector.x = Input.GetAxis("Horizontal") * speedBoxOne;
-     boxVector.z = Input.GetAxis("Vertical") * speedBoxOne;
+        boxVector = Vector3.zero;
+        boxVector.x = -Input.GetAxis("Horizontal") * speedBoxOne;
+        boxVector.z = -Input.GetAxis("Vertical") * speedBoxOne;
 
+        if (boxVector.x != 0 || boxVector.z != 0)
+            ch_BoxAnimation.SetBool("Move", true);
+        else ch_BoxAnimation.SetBool("Move", false);
+        
         if (Vector3.Angle(Vector3.forward, boxVector) > 1f)
         {
             Vector3 direct = Vector3.RotateTowards(transform.forward, boxVector, speedBoxOne, 0.0f);
             transform.rotation = Quaternion.LookRotation(direct);
         }
 
-        ch_BoxController.Move(boxVector * Time.deltaTime);// Метод движения по направлению
+        ch_BoxController.Move(-boxVector * Time.deltaTime);// Метод движения по направлению
 
     }
 
